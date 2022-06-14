6dd5b8d32d7e7fa9b3c3980e16169ddbf4bf7cd8
aqui <aquiabuin@gmail.com>
2022-06-13 23:38:26 -0300

    agregue mapa

---
+++ src/main/test/edu/fiuba/algo3/modelo/MapaTest.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.modelo;
+import org.junit.jupiter.api.Test;
+
+public class MapaTest {
+	@Test
+	public void logTest() {
+		Mapa mapa = new Mapa();
+		System.out.print(mapa.calles);
+	}
+}

6aae7a92f876e6a03d0f994819b2beeb2e6a93af
pgallino <90009211+pgallino@users.noreply.github.com>
2022-06-22 20:18:58 -0300

    -

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,10 +1,9 @@
 package edu.fiuba.algo3;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.FileNotFoundException;
 
 /**
  * JavaFX App
@@ -12,12 +11,7 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-
-        var label = new Label("Hello, JavaFX ");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws FileNotFoundException {
     }
 
     public static void main(String[] args) {
--- src/main/java/edu/fiuba/algo3/modelo/Estado.java
+++ src/main/java/edu/fiuba/algo3/modelo/Estado.java
@@ -16,7 +16,6 @@ public abstract class Estado {
     public abstract void pasarPozo();
 
     public void pasarVacio() {
-        return;
     }
 }
 

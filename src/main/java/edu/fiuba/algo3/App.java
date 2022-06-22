230a4b1cc6c822ef8f888b6943c7437fb92e99e9
aqui <aquiabuin@gmail.com>
2022-06-22 20:07:56 -0300

    auto se mueve

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -11,29 +11,31 @@ import javafx.scene.paint.Color;
 import javafx.scene.image.*;
 import javafx.scene.shape.Rectangle;
 import javafx.geometry.Insets;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import edu.fiuba.algo3.modelo.*;
 
 
 public class App extends Application {
+    private static final int TAMANIO_MANZANA = 35;
+    private static final int TAMANIO_CALLE = 15;
 
     @Override
     public void start(Stage stage) throws FileNotFoundException {
+        
         Juego juego = new Juego();
         juego.aplicarEstado(new Auto(juego.vehiculo));
         GridPane gp = new GridPane();
         
-        gp.setPadding( new Insets(10) );
         gp.setHgap( 15 );
         gp.setVgap( 15 );
+
         Image image = new Image(new FileInputStream("assets/car.png"));
         ImageView imageView = new ImageView(image);
-        imageView.setX(0);
-        imageView.setY(0);
+
         Pane pane = new Pane(gp, imageView); 
         var scene = new Scene(pane);
+
         scene.setOnKeyPressed(e-> {
             if (e.getCode() == KeyCode.UP) {
                 juego.mover(new DireccionArriba());
@@ -44,16 +46,19 @@ public class App extends Application {
             } else if (e.getCode() == KeyCode.RIGHT) {
                 juego.mover(new DireccionDerecha());
             }
+            imageView.setX(juego.vehiculo.posicion.x * (TAMANIO_MANZANA + TAMANIO_CALLE));
+            imageView.setY(juego.vehiculo.posicion.y * (TAMANIO_MANZANA + TAMANIO_CALLE));
             System.out.print(juego.vehiculo.posicion.x);
             System.out.print(",");
             System.out.print(juego.vehiculo.posicion.y);
             System.out.print("\n");
+            System.out.print(juego.mapSize);
         });
         for (int i = 0; i < juego.mapSize; i++) {
             for (int j = 0; j < juego.mapSize; j++) {
                 Rectangle r = new Rectangle();
-                r.setHeight(35);
-                r.setWidth(35);
+                r.setHeight(TAMANIO_MANZANA);
+                r.setWidth(TAMANIO_MANZANA);
                 r.setArcWidth(10);
                 r.setArcHeight(10);
                 r.setFill(Color.GRAY);

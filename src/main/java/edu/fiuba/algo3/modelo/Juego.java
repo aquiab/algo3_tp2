0d5895f817aca37462587c3dd5d42684a27831f5
aqui <aquiabuin@gmail.com>
2022-06-23 19:26:36 -0300

    comente test unitario

--- src/main/java/edu/fiuba/algo3/modelo/Juego.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego.java
@@ -7,7 +7,7 @@ public class Juego {
 	private Integer MOVIMIENTOS_INICIALES = 0;
 
 	public Ranking ranking = new Ranking();
-	public int mapSize = ThreadLocalRandom.current().nextInt(10, 20);
+	public int mapSize = ThreadLocalRandom.current().nextInt(10, 15);
 	public Mapa mapa = new Mapa(mapSize);
 
 	public Vehiculo vehiculo = new Vehiculo(MOVIMIENTOS_INICIALES, new Posicion(POSICION_INICIAL, POSICION_INICIAL, this.mapa));

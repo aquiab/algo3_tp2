ebff50c0e54adcf18d8a2f659bcfde80f2529151
ValentinBrizuela <vabrizuela@fi.uba.ar>
2022-07-05 15:06:25 -0300

    pantalla de victoria actualizada

--- src/main/java/edu/fiuba/algo3/view/ContenedorVictoria.java
+++ src/main/java/edu/fiuba/algo3/view/ContenedorVictoria.java
@@ -1,28 +1,55 @@
 package edu.fiuba.algo3.view;
 import edu.fiuba.algo3.modelo.*;
 import edu.fiuba.algo3.modelo.constructor_juego.JuegoDirector;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.*;
 import javafx.geometry.Pos;
+import javafx.scene.text.Font;
+
 import java.util.ArrayList;
 
 public class ContenedorVictoria extends VBox {
 	public ContenedorVictoria(JuegoDirector director) {
 		Juego juego = director.obtenerPartida();
+        Jugador jugador = juego.ranking.devolverGanador();
+
 		Button botonVolver = new Button("Volver");
+        botonVolver.relocate(20, 460);
+
         VBox vboxizq = new VBox();
         VBox vboxder = new VBox();
-        HBox hbox = new HBox(50, botonVolver, vboxizq, vboxder);
-        hbox.setPrefSize(350, 500);
-        hbox.setAlignment(Pos.TOP_CENTER);
+        HBox hbox = new HBox(50, vboxizq, vboxder);
+        hbox.relocate(175, 150);
+
+        Label felicidades = new Label();
+        felicidades.setText("Felicidades!");
+        felicidades.relocate(165, 20);
+        felicidades.setFont(new Font(30));
+
+        Label llegaste = new Label();
+        llegaste.setText("Llegaste a la meta con un puntaje de " + String.valueOf(jugador.obtenerPuntaje()));
+        llegaste.relocate(110, 60);
+        llegaste.setFont(new Font(15));
+
+        Label ranking = new Label();
+        ranking.setText("Ranking");
+        ranking.relocate(205, 110);
+        ranking.setFont(new Font(20));
+
+        Pane pane = new Pane(hbox, botonVolver, felicidades, ranking, llegaste);
+        pane.setPrefSize(350, 500);
+        pane.setStyle("-fx-background-color: #ec6553");
 
         ArrayList<Jugador> jugadores = new ArrayList<>();
         vboxizq.getChildren().clear();
         vboxder.getChildren().clear();
         vboxizq.getChildren().add(new Label("Jugador"));
         vboxder.getChildren().add(new Label("Puntaje"));
-        Jugador jugador = juego.ranking.devolverGanador();
+
         while (jugador != null) {
             vboxizq.getChildren().add(new Label(jugador.nombre));
             vboxder.getChildren().add(new Label(String.valueOf(jugador.movimientos)));
@@ -32,9 +59,9 @@ public class ContenedorVictoria extends VBox {
         for (int i = 0; i < jugadores.size(); i++) {
             juego.ranking.agregarJugador(jugadores.get(i));
         }
-        this.getChildren().add(hbox);
+        this.getChildren().add(pane);
         botonVolver.setOnAction(e -> {
-			//this.getScene().setRoot(new ContenedorMenu(director));
+			this.getScene().setRoot(new ContenedorMenu(director));
 		});
 	}
 }
